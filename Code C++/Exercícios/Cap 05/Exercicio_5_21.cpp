f425956d52f0f2c5e2c2a153150ffe6bc3c7f0e2
SanDiegoCastilho <sandiegocastilho@gmail.com>
2019-03-01 16:15:45 -0300

    Finalizado 5.21

--- Code C++/Exercícios/Cap 05/Exercicio_5_21.cpp
+++ Code C++/Exercícios/Cap 05/Exercicio_5_21.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int main(){
-	int Type, Hours, HourValue;
-	double SalManager = 0, SalHours = 0;
+	int Type, Hours, HourValue, SalesValue, Qtde;
+	double SalManager = 0, SalHours = 0, SalComm = 0, ProductValue = 0, SalProduction = 0;
 
 
 	cout << "Digite o código do empregado (-1 para sair): ";
@@ -15,32 +15,47 @@ int main(){
 	while(Type != -1){
 		switch(Type){
 			case 1: //Gerente
-				cout << "Digite o valor do salário do Gerente (R$): " << endl;
+				cout << "Digite o valor do salário do Gerente (R$): ";
 				cin >> SalManager;
-				cout << "Salário gerente (R$): " << SalManager << endl; 
+				cout << "Salário gerente: R$ " << SalManager << endl;
 				break;
 
 			case 2: //Horistas
-				cout << "Digite a quantidade de horas trabalhadas: " << endl;
+				cout << "Digite a quantidade de horas trabalhadas: ";
 				cin >> Hours;
 
-				cout << "Digite o valor da hora (R$): " << endl;
+				cout << "Digite o valor da hora (R$): ";
 				cin >> HourValue;
 
 				if (Hours <= 40)
 					SalHours = Hours * HourValue;
 
 				else
-					
+					Hours = Hours - 40;
+					SalHours = (40 * HourValue) + (Hours * 1.5 * HourValue);					
 				
-				SalHours = SalHours + ((Hours) * HourValue) + 
-
+				cout << "Salário horistas (R$): " << SalHours << endl;
 				break;
 
-			case 3:
+			case 3: //Comissionados
+				cout << "Digite o valor bruto das vendas (R$): ";
+				cin >> SalesValue;
+
+				SalComm = 250 + (0.57 * SalesValue);
+
+				cout << "Salário comissionados: R$ " << SalComm << endl;
 				break;
 
-			case 4:
+			case 4: //Trabalhadores por produção
+				cout << "Digite a quantidade de produtos produzidos: ";
+				cin >> Qtde;
+
+				cout << "Digite o valor do produto (R$): ";
+				cin >> ProductValue;
+
+				SalProduction = ProductValue * Qtde;
+
+				cout << "Salário do trabalhados p/ produção: R$ " << SalProduction << endl;
 				break;
 
 			case ' ':
